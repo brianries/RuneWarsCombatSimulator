ea95ff668f720430346c6bd2c0e9ae653a110c5b
dsayles <dsayles@walleyesoftware.com>
2017-10-01 12:23:44 -0500

    adding daqan figure upgrades (music)

---
+++ src/rwcsim/factions/daqan/upgrades/figureupgrades/infantry/MarchingCornicen.java
@@ -0,0 +1,12 @@
+package rwcsim.factions.daqan.upgrades.figureupgrades.infantry;
+
+import rwcsim.factions.base.FigureUpgrade;
+import rwcsim.factions.base.InfantryUpgrade;
+import rwcsim.factions.base.upgrades.Music;
+import rwcsim.factions.waiqar.upgrades.figureupgrades.infantry.MarchingDrummer;
+
+public class MarchingCornicen extends FigureUpgrade implements Music, InfantryUpgrade {
+    public MarchingCornicen() {
+        this.armor = 2;
+    }
+}
---
+++ src/rwcsim/factions/daqan/upgrades/figureupgrades/infantry/RallyingCornicen.java
@@ -0,0 +1,9 @@
+package rwcsim.factions.daqan.upgrades.figureupgrades.infantry;
+
+import rwcsim.factions.base.FigureUpgrade;
+import rwcsim.factions.base.InfantryUpgrade;
+import rwcsim.factions.base.upgrades.Music;
+
+public class RallyingCornicen extends FigureUpgrade implements Music, InfantryUpgrade {
+    public RallyingCornicen() {}
+}
